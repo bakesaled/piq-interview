befbb257b76b790b12057220d8024d7be425441a
Brandon Eleuterio <eleuterio@railroad19.com>
2018-02-26 19:25:21 -0700

    connecting to db

--- src/server/app-server.ts
+++ src/server/app-server.ts
@@ -2,6 +2,8 @@ import { Application } from 'express';
 import * as express from 'express';
 import bodyParser = require('body-parser');
 import { DefaultConfig } from './config/default';
+import * as mongoose from 'mongoose';
+import Q = require('q');
 
 export class AppServer {
   public app: Application;
@@ -16,6 +18,8 @@ export class AppServer {
   }
 
   public init() {
+    this.initDb();
+
     console.log('listening on port', DefaultConfig.port);
     this.app.listen(DefaultConfig.port);
     this.app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,5 +30,16 @@ export class AppServer {
     this.app.get('/', function(req, res) {
       res.sendStatus(200);
     });
+  }
+
+  private initDb() {
+    global.Promise = Q.Promise;
+    (<any>mongoose).Promise = global.Promise;
+    const connection: mongoose.Connection | any = mongoose.createConnection(
+      DefaultConfig.database,
+      DefaultConfig.dbOptions
+    );
+
+    console.log(`connected to '${connection.name}' on '${connection.host}'`);
   }
 }
--- src/server/config/default.ts
+++ src/server/config/default.ts
@@ -1,3 +1,11 @@
+import { ConnectionOptions } from 'mongoose';
+
 export class DefaultConfig {
   public static port: any = process.env.ATH_API_PORT || 7000;
+  public static database: string = process.env.CT_API_DATABASE ||
+    'mongodb://testuser:1234@ds249428.mlab.com:49428/athenaeum';
+  public static dbOptions: ConnectionOptions = {
+    keepAlive: 300000,
+    connectTimeoutMS: 30000
+  };
 }
